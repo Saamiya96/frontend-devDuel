9b40c8ebf8d2aea5ca3c9555729c63877a9375be
Adrian Hards <github@ahards.com>
2023-07-03 20:17:02 +0100

    setup react with vite

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { ICharacter } from "./components/cards/cardTypes";
 import Card from "./components/cards/Card";
-import characters from "./data/cards.json";
+import characters from "./data/testCards.json";
 import "./styles/App.css";
 
 function App() {
