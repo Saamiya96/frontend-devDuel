b18f88a5a79af599c5c7520578c9dbe82beac75a
Adrian Hards <github@ahards.com>
2023-07-04 17:31:58 +0100

    click on card shows stat selected

--- src/components/cards/CardStats.tsx
+++ src/components/cards/CardStats.tsx
@@ -2,13 +2,14 @@ import { IStats } from "./cardTypes";
 
 interface CardStatsProps {
   stats: IStats;
+  onStatSelect: (statValue: number) => void;
 }
 
-function CardStats({ stats }: CardStatsProps) {
+function CardStats({ stats, onStatSelect }: CardStatsProps) {
   return (
     <div className="card-stats">
       {Object.entries(stats).map(([key, value], i) => (
-        <div key={i}>
+        <div key={i} onClick={() => onStatSelect(value)}>
           <strong>{key}:</strong> {value}
         </div>
       ))}
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import Card from "../components/cards/Card";
 import languages from "../data/testCards.json";
 import { ILanguage } from "../components/cards/cardTypes";
 
 function CardList() {
+  const [selectedStatValue, setSelectedStatValue] = useState<number | null>(
+    null
+  );
+
   return (
     <div>
       {languages.map((language: ILanguage) => (
-        <Card key={language.id} language={language} />
+        <Card
+          key={language.id}
+          language={language}
+          onStatSelect={setSelectedStatValue}
+        />
       ))}
+      {selectedStatValue !== null && (
+        <p>Selected stat value: {selectedStatValue}</p>
+      )}
     </div>
   );
 }
