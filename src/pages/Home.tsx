5238359e739f5d5aca509819a33edf463d1450d7
Adrian Hards <github@ahards.com>
2023-07-04 14:31:18 +0100

    remove old tailwind config

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div style={{ padding: 20 }}>
-      <h2>Home View</h2>
+      <h2 className="text-3xl">Home View</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
     </div>
   );
