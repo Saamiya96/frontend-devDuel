a1d06a147cf5d4b1243de60ef9784fd6a5ab6d11
Adrian Hards <github@ahards.com>
2023-07-04 09:34:22 +0100

    add basic navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
+import { ICharacter } from "./components/cards/cardTypes";
+import Card from "./components/cards/Card";
+import characters from "./data/testCards.json";
 
 function App() {
   return (
     <Router>
       <nav>
-        <ul>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
       </nav>
+      <div className="app">
+        {characters.map((character: ICharacter) => (
+          <Card key={character.id} character={character} />
+        ))}
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
