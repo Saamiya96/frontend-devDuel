1d2f69ca67b70c24c07940219d8788cfe54ccb45
Adrian Hards <github@ahards.com>
2023-07-05 18:17:47 +0100

    switch stat selector to key

--- src/components/cards/CardStats.tsx
+++ src/components/cards/CardStats.tsx
@@ -2,14 +2,14 @@ import { IStats } from "./cardTypes";
 
 interface CardStatsProps {
   stats: IStats;
-  onStatSelect: (statValue: number) => void;
+  onStatSelect: (statValue: string) => void;
 }
 
 function CardStats({ stats, onStatSelect }: CardStatsProps) {
   return (
     <div className="card-stats">
       {Object.entries(stats).map(([key, value], i) => (
-        <div key={i} onClick={() => onStatSelect(value)}>
+        <div key={i} onClick={() => onStatSelect(key)}>
           <strong>{key}:</strong> {value}
         </div>
       ))}
