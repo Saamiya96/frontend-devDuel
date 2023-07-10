09f9c1316391c095d30722f368c4a56c292f0d8f
Adrian Hards <github@ahards.com>
2023-07-10 23:20:22 +0100

    only adds username to waiting room through click

--- src/pages/WaitingRoom.tsx
+++ src/pages/WaitingRoom.tsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react";
+import { useWebSocket } from "../hooks/useWebSocket";
+
 function WaitingRoom() {
-  return <h1>Waiting Room</h1>;
+  const [users, setUsers] = useState<string[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/waiting_room")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const socket = useWebSocket();
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("username", (data) => {
+        setUsers((prevUsers) => [...prevUsers, data.username]);
+      });
+    }
+    return () => {
+      if (socket) {
+        socket.off("username");
+      }
+    };
+  }, [socket]);
+
+  return (
+    <div>
+      <h1>Waiting Room</h1>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>{user}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default WaitingRoom;
