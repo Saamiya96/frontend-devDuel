769bfddd05cb1b0745543f0244ef0129e2de0a5a
Jasmine <j.asra.harper@gmail.com>
2023-07-11 10:45:28 +0100

    added waiting room logic

--- src/pages/WaitingRoom.tsx
+++ src/pages/WaitingRoom.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useWebSocket } from "../hooks/useWaitingSocket";
+import { Link } from "react-router-dom";
 
 function WaitingRoom() {
   const [users, setUsers] = useState<string[]>([]);
@@ -25,14 +26,36 @@ function WaitingRoom() {
     };
   }, [socket]);
 
+  useEffect(() => {
+    if (socket) {
+      socket.on("update_users", (updatedUsers) => {
+        setUsers(updatedUsers);
+      });
+    }
+    return () => {
+      if (socket) {
+        socket.off("update_users");
+      }
+    };
+  }, [socket]);
+
   return (
     <div>
       <h1>Waiting Room</h1>
+      <p>Current Players:</p>
       <ul>
         {users.map((user, index) => (
           <li key={index}>{user}</li>
         ))}
       </ul>
+      {users.length === 2 && (
+        <Link to="/game">
+          <button>Start Game</button>
+        </Link>
+      )}
+      {users.length < 2 && (
+        <p>Waiting for another player...</p>
+      )}
     </div>
   );
 }
